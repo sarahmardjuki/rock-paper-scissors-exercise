e8541a5ecb56af84af2697dbda161d9226eb8e29
sarahmardjuki <77697165+sarahmardjuki@users.noreply.github.com>
2021-02-03 14:45:27 -0800

    Simulate computer choice

--- game.py
+++ game.py
@@ -1,22 +1,32 @@
 # game.py
 
+import random # load the module to avoid `NameError: name 'random' is not defined`
+
+# intro text
+
 print("Rock, Paper, Scissors, Shoot!")
 
 print("-------------------")
 print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 print("-------------------")
 
+
 # asking user for an input
-x = input("Please choose either 'rock', 'paper', or 'scissors': ")
-print("You chose: ", x)
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
+print("You chose: ", user_choice)
+
 
 # validate user inputs (case insensitive)
-if x.lower() != "rock" and x.lower() != "paper" and x.lower() != "scissors":
+if user_choice.lower() != "rock" and user_choice.lower() != "paper" and user_choice.lower() != "scissors":
     print("Please select either 'rock', 'paper', or 'scissors'. Thanks!")
     exit()
 
-print("Done!")
-exit()
+
+# simulate computer selection
+computer_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(computer_options)
+print("The computer chose: ", computer_choice)
+
 
 
 
