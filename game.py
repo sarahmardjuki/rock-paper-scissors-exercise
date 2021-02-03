3a12f1f4d40e94d8498c906c158797cef791b9a2
sarahmardjuki <77697165+sarahmardjuki@users.noreply.github.com>
2021-02-03 14:39:57 -0800

    Ask user for inputs and validate them

--- game.py
+++ game.py
@@ -1,3 +1,27 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+# asking user for an input
+x = input("Please choose either 'rock', 'paper', or 'scissors': ")
+print("You chose: ", x)
+
+# validate user inputs (case insensitive)
+if x.lower() != "rock" and x.lower() != "paper" and x.lower() != "scissors":
+    print("Please select either 'rock', 'paper', or 'scissors'. Thanks!")
+    exit()
+
+print("Done!")
+exit()
+
+
+
+
+
+
+
+
